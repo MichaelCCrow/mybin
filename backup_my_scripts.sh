454eeb46c083f8f9627d962ba0174145f04a44dc
Crow, Michael C <crowmc@ornl.gov>
2017-11-16 02:00:00 -0500

    2017-11-16@02:00 commit of my custom scripts

--- backup_my_scripts.sh
+++ backup_my_scripts.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-#backup script to copy all of the directories I have created scripts in - including my custom bin
+#ABOUT:
+# backup script to copy all of the directories I have created scripts in - including my custom bin
+#:ABOUT
+
+confirmgit=true
+if [[ $# = 1 ]]
+then if [[ "$1" = "nogit" ]];
+then confirmgit=false; echo "skipping git backup"
+#else echo "nogit is the only option - quitting"; exit
+fi; fi
 
 home="/Users/mcu"
 mybin="/Users/mcu/Development/mybin"
@@ -17,7 +26,7 @@ dobak $home/Development/scripts
 dobak $home/.bash_profile "bash_profile"
 dobak $home/.bash_aliases "bash_aliases"
 dobak $home/.vimrc "vimrc"
-if [[ $# > 0 ]]; then for i in ${@}; do dobak $i; done; fi
+if [[ $# > 0 ]]; then for i in ${@}; do if [[ $i != "nogit" ]]; then dobak $i; fi done; fi
 
 dogit() {
     git status
@@ -29,7 +38,7 @@ dogit() {
 
 if [ -n "$(cat differences.txt)" ]; then
     rm differences.txt
-	dogit
+    if [ $confirmgit ]; then dogit; fi
 else 
     rm differences.txt
 fi
