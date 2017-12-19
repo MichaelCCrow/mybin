99a578c737f2fedeab6fa547842aef576c4384d3
Crow, Michael C <crowmc@ornl.gov>
2017-12-19 12:00:01 -0500

    2017-12-19@12:00 commit of my custom scripts

--- bin/runchecks-checknga.py
+++ bin/runchecks-checknga.py
@@ -7,7 +7,9 @@ file = ET.parse(path)
 status = file.find("ome/ome_status")
 rid = file.find("ome/record_id")
 origins = file.find("idinfo/citation/citeinfo")
+projloc = file.find("idinfo/spdom/descgeog")
 
 print(' <'+status.tag + ">" + status.text + " -- <" + rid.tag + ">" + rid.text)
 for origin in origins.findall('origin'):
     print("\t <origin>" + origin.text)
+if projloc is not None: print('<descgeog>',projloc.text)
